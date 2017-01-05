deeed68a4e78b866c8a96c059466be11e2da0479
Dorian C Brown <brown.3794@gmail.com>
2017-01-05 15:41:12 -0500

    Removed all pull data functions

--- src/gamayun/core.clj
+++ src/gamayun/core.clj
@@ -1,49 +1,14 @@
 (ns gamayun.core)
 
 
-(defn -main
-  [& args]
-  (println "Hello, World!"))
-
-(def id-count 0)
-
-(defn def-keyvalue
-  [m fn-name]
- (:doc (meta (symbol (str "#'clojure.core/" (get m fn-name))))))
+(def clj-core-map
+  (ns-map 'clojure.core))
 
 
-(def deck-structure {:deck {:card {:fm-name "map"
-                                   :fm-def "Returns a lazy sequence consisting of the result of applying f to
-                                            the set of first items of each coll, followed by applying f to the
-                                            set of second items in each coll, until any one of the colls is
-                                            exhausted."
-                                   :fm-args "(map f coll)"}}})
-(defn add-card [map card]
- (assoc deck-structure :deck card))
+(defn transform-vals [map f & rkeys]
+  (reduce-kv (fn [map k v]
+              (assoc map k (f v))) {} map))
 
-(defn remove-card [card]
- (dissoc deck-structure card))
-
-
-(def example-card {:id (inc id-count)
-                   :fuctname 'map
-                   :args '[funct (coll & rest)]
-                   :def (def-keyvalue example-card :fuctname)
-                   :example '(map + [4 5 6] [1 2 3])})
-
-(def example-deck [ example-card example-card example-card])
-
-(defn pull-meta-data [nspace]
-  (map meta (vals (ns-map nspace))))
-
-(defn pull-fm-name [nspace]
-  (remove nil? (map :name (pull-meta-data nspace))))
-
-(defn pull-fm-args [nspace]
-  (remove nil? (map :arglists (pull-meta-data nspace))))
-
-(defn pull-fm-docs [nspace]
-  (remove nil? (map :doc (pull-meta-data nspace))))
 
 (defn name-kw? [map]
   (contains? map :name))
@@ -68,9 +33,3 @@
 
 (defn create-card-map [namespace]
   (zipmap (create-card-num-keywords namespace) (filter-by-doc namespace)))
-
-(def clj-core-map
- (create-card-map 'clojure.core))
-
-(def clj-core-list
-  (filter-by-args 'clojure.core))
