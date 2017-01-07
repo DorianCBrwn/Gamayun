68bbf59e75c0f8c6c18396fcda0eb4e977fe2e88
Dorian C Brown <brown.3794@gmail.com>
2017-01-07 17:57:35 -0500

    add refactored deck-map function

--- src/gamayun/core.clj
+++ src/gamayun/core.clj
@@ -3,14 +3,19 @@
 (defn transform-vals [map f]
   "apply given function to all values in a map"
   (reduce-kv (fn [map k v]
-              (assoc map k (f v))) {} map))
+               (assoc map k (f v))) {} map))
 
 (defn keywordize-symbols [m]
   "Turns symbol keys into keywords"
   (reduce-kv (fn [m k v]
                (assoc m (keyword k) v)) {} m ))
 
+(defn deck-map [ns]
+  "creates a deck  map data structure based provided of the provided namespace"
+  (keywordize-symbols (transform-vals (ns-map ns) meta)))
+
 (defn remove-vals [ns k]
   "remove keys from specific keys"
   (update-in (deck-map ns) [k] dissoc :file :line :column :ns :static))
+
 
